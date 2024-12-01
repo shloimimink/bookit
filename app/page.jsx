05fe62b96b854835cd4fbf391bfb16a69ff5a2b9
Shloimi Minkowicz <shloimiminkowicz@Shloimis-MacBook-Pro.local>
2024-11-30 20:12:40 -0600

    final commit

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+import Heading from "@/components/Heading";
+import RoomCard from "@/components/RoomCard";
+import getAllRooms from "./actions/getAllRooms";
+
+export default async function Home() {
+  const rooms = await getAllRooms();
+
+  return (
+    <>
+      <Heading title="Available Rooms" />
+      {rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard room={room} key={room.$id} />)
+      ) : (
+        <p>No rooms available at the moment</p>
+      )}
+    </>
+  );
+}
